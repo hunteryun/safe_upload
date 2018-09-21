be5b440a75eeefed93ca73b9812ed8edef977ac6
aviggngyv <aviggngyv@gmail.com>
2018-09-21 13:58:27 +0800

    fix

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -6,7 +6,7 @@ use Zend\Diactoros\ServerRequest;
 use Psr\Http\Message\UploadedFileInterface;
 use Zend\Diactoros\Response\JsonResponse;
 use Rundiz\Upload\Upload;
-use Thumbnailr\Thumbnailr;
+use Gregwar\Image\Image;
 
 /**
  * Class Upload.
@@ -21,12 +21,19 @@ class UploadController {
    *   Return safe_upload string.
    */
   public function safe_upload(ServerRequest $request) {
-    $result = false;
+    global $safe_key;
+    $result = t('Upload Failed');
     $notvalidate = false;
     $allowed_file = array();
     $uploadedFiles = $request->getUploadedFiles();
     $parms = $request->getParsedBody();
     $query_parms = $request->getQueryParams();
+
+    if($request->getUri()->getPath() == '/public/safe/upload'){
+      if(!isset($query_parms['safe_key']) || $query_parms['safe_key'] != $safe_key){
+        return new JsonResponse(array('code' => 1, 'msg' => $result, "data" => []));
+      }
+    }
 
     if(!isset($parms['accept']) || empty($parms['accept'])) {
       $parms['accept'] = 'images';
@@ -42,7 +49,7 @@ class UploadController {
         $parms['exts'] = 'jpg|png|gif|bmp|jpeg';
       }
       if(!empty(array_diff(explode('|', $parms['exts']), array('jpg','png','gif','bmp','jpeg','tif')))){
-        return new JsonResponse($result);
+        return new JsonResponse(array('code' => 1, 'msg' => $result, "data" => []));
       }
       $allowed_file = explode('|', $parms['exts']);
       break;
@@ -51,7 +58,7 @@ class UploadController {
         $parms['exts'] = 'doc|pdf|txt|xls|zip|rar';
       }
       if(!empty(array_diff(explode('|', $parms['exts']), array('doc','pdf','txt','xls','zip','rar','7z','obj','pmd','vmd','mp3','mp4','jpg','gif','bmp','xml','dat')))){
-        return new JsonResponse($result);
+        return new JsonResponse(array('code' => 1, 'msg' => $result, "data" => []));
       }
       $allowed_file = explode('|', $parms['exts']);
       break;
@@ -60,7 +67,7 @@ class UploadController {
         $parms['exts'] = 'mp4|flv|mpg|3gp';
       }
       if(!empty(array_diff(explode('|', $parms['exts']), array('rm','rmvb','wmv','avi','mp4','3gp','mkv','flv','mpg')))){
-        return new JsonResponse($result);
+        return new JsonResponse(array('code' => 1, 'msg' => $result, "data" => []));
       }
       $allowed_file = explode('|', $parms['exts']);
       break;
@@ -69,12 +76,12 @@ class UploadController {
         $parms['exts'] = 'mp3|wma|wav';
       }
       if(!empty(array_diff(explode('|', $parms['exts']), array('wav','mp3','ogg','wma','aac')))){
-        return new JsonResponse($result);
+        return new JsonResponse(array('code' => 1, 'msg' => $result, "data" => []));
       }
       $allowed_file = explode('|', $parms['exts']);
       break;
     default:
-      return new JsonResponse($result);
+      return new JsonResponse(array('code' => 1, 'msg' => $result, "data" => []));
     }
 
     if (!empty($uploadedFiles)) {
@@ -84,7 +91,7 @@ class UploadController {
         }
       }
     }
-    return new JsonResponse($result);
+    return $result;
   }
 
   /**
@@ -94,7 +101,7 @@ class UploadController {
    *   Return safe_upload string.
    */
   private function safe_upload_file($field_name, $value, $allowed_file = array('gif', 'jpg', 'jpeg', 'png'), $notvalidate = FALSE, $query_parms = array()) {
-    global $auto_image_compress, $base_url;
+    global $auto_image_compress, $image_style, $base_url;
     $module = '';
     if(empty($module)) {
       $module = substr($field_name,0,strpos($field_name,'-'));
@@ -104,17 +111,9 @@ class UploadController {
     $msg = '';
     $Upload = new Upload($field_name);
     $move_dir = 'sites/upload/'.$field_name.'/';
-    $thumb_move_dir = 'sites/thumb/'.$field_name.'/';
-    if(!empty($module) && module_exists($module)){
-      $move_dir = 'sites/upload/'.$module.'/'.str_replace("$module-","",$field_name).'/';
-      $thumb_move_dir = 'sites/thumb/'.$module.'/'.str_replace("$module-","",$field_name).'/';
-    }
     $Upload->move_uploaded_to = $move_dir;
     if (!is_dir($move_dir)){
       mkdir($move_dir, 0755, true);
-    }
-    if (!is_dir($thumb_move_dir)){
-      mkdir($thumb_move_dir, 0755, true);
     }
     // Allowed for gif, jpg, png
     $Upload->allowed_file_extensions = $allowed_file;
@@ -140,18 +139,57 @@ class UploadController {
     // Get the uploaded file's data.
     $uploaded_data = $Upload->getUploadedData();
 
+    //Create upload dir
+    $thumb_group = array();
+
+    if(empty($image_style) || (!isset($query_parms['style']) || !isset($image_style[$query_parms['style']]))){
+      $thumb_group['thumb']['thumb_move_dir'] = 'sites/thumb/'.$field_name.'/';
+      $thumb_group['thumb']['width'] = '90';
+      $thumb_group['thumb']['height'] = '90';
+      if (!is_dir($thumb_group['thumb']['thumb_move_dir'])){
+        mkdir($thumb_group['thumb']['thumb_move_dir'], 0755, true);
+      }
+    }else {
+      if(is_array($image_style[$query_parms['style']])) {
+        foreach ($image_style[$query_parms['style']] as $dir => $thumb_size) {
+          $thumb_group[$dir]['thumb_move_dir'] = 'sites/thumb/'.$dir.'/'.$field_name.'/';
+          $size = explode('*',$thumb_size['size']);
+          $thumb_group[$dir]['width'] = $size[0];
+          $thumb_group[$dir]['height'] = $size[1];
+          $thumb_group[$dir]['method'] = $thumb_size['method'];
+          $thumb_group[$dir]['background'] = $thumb_size['background'];
+          if (!is_dir($thumb_group[$dir]['thumb_move_dir'])){
+            mkdir($thumb_group[$dir]['thumb_move_dir'], 0755, true);
+          }
+        }
+      }else {
+        $thumb_group[$query_parms['style']]['thumb_move_dir'] = 'sites/thumb/'.$query_parms['style'].'/'.$field_name.'/';
+        $size = explode('*',$image_style[$query_parms['style']]);
+        $thumb_group[$query_parms['style']]['width'] = $size[0];
+        $thumb_group[$query_parms['style']]['height'] = $size[1];
+        $thumb_group[$query_parms['style']]['method'] = 'resize';
+        $thumb_group[$query_parms['style']]['background'] = '#FFFFFF';
+        if (!is_dir($thumb_group[$query_parms['style']]['thumb_move_dir'])){
+          mkdir($thumb_group[$query_parms['style']]['thumb_move_dir'], 0755, true);
+        }
+      }
+    }
+
     if (is_array($uploaded_data) && !empty($uploaded_data)) {
       foreach ($uploaded_data as $key => $item) {
         $url = $move_dir.$uploaded_data[$key]['new_name'];
         if(in_array(strtolower($uploaded_data[$key]['extension']), array('jpg','jpeg','png'))){
-          $thumburl = $thumb_move_dir.$Upload->new_file_name.'.'.$uploaded_data[0]['extension'];
-          $thumbnailr = new Thumbnailr($url);
-          $thumbnailr->buildThumbnail('70', '70');
-          $thumbnailr->toJpegFile($thumburl);
+          foreach ($thumb_group as $thumb_key => $thumb_item) {
+            $thumburl = $thumb_item['thumb_move_dir'].$Upload->new_file_name.'.'.$uploaded_data[0]['extension'];
+            $method = $thumb_item['method'];
+            Image::open($url)
+                 ->$method($thumb_item['width'], $thumb_item['height'], $thumb_item['background'])
+                 ->save($thumburl);
 
-          if(isset($query_parms['compress']) && ($query_parms['compress'] == 'yes' || $query_parms['fullpath'] == 'true')){
-            if(module_exists('image_compress') && $auto_image_compress['enable']){
-              hunter_compress_image($url, $url, $auto_image_compress['quality']);
+            if(isset($query_parms['compress']) && ($query_parms['compress'] == 'yes' || $query_parms['fullpath'] == 'true')){
+              if(module_exists('image_compress') && $auto_image_compress['enable']){
+                hunter_compress_image($url, $url, $auto_image_compress['quality']);
+              }
             }
           }
         }
